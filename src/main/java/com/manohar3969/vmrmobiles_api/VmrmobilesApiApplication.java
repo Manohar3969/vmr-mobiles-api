b3ccfddbde2df07eeb2d44a1be162384962b7544
Manohar V <reddymanohar3969@gmail.com>
2024-12-26 15:18:53 +0530

    VMR Mobiles API Services - Initial Code

---
+++ src/main/java/com/manohar3969/vmrmobiles_api/VmrmobilesApiApplication.java
@@ -0,0 +1,13 @@
+package com.manohar3969.vmrmobiles_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmrmobilesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmrmobilesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manohar3969/vmrmobiles_api/VmrmobilesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manohar3969.vmrmobiles_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmrmobilesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
